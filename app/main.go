9e6faee0baf6fb28ec2d710a17b9472dc8848f59
Luiz Guilherme 2 <luizguilhermebarbosa109@gmail.com>
2025-07-30 18:41:01 -0300

    changes

--- app/main.go
+++ app/main.go
@@ -54,8 +54,8 @@ func main() {
 			// // io.Copy(os.Stdout, string(rc))
 			r.Close()
 			valueFound := strings.Index(string(rc), "\x00")
-			fmt.Println(valueFound)
-			// fmt.Printf("%s", string(rc)[valueFound:])
+
+			fmt.Printf("%s", string(rc)[valueFound+4:])
 			// for i := range len(rc) {
 			// 	if rc[i] == "\x00" {
 			// 		fmt.Printf("%s", string(rc)[i:])
